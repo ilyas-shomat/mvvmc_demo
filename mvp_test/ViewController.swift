0993a2d7d10785ccac5844c8000abf7dabf554f4
Ilyas Shomat <ilyas.shomat@gmail.com>
2021-03-05 17:51:25 +0600

    Initial Commit

---
+++ mvp_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvp_test
+//
+//  Created by Ilyas Shomat on 05.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
